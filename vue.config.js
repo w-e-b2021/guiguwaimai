b9bb5891f726e1987e1cb0acc7671d35d79c0d41
xzw <2283341909@qq.com>
2022-05-05 21:14:30 +0800

    the second

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5500',
+        pathRewrite: { '^/api': '' },
+        ws: true, //用于支持websocket
+        changeOrigin: true //撒谎
+      }
+    }
+  },
+  pages: {
+    index: {
+      entry: 'src/main.js'
+    }
+  }
 })
